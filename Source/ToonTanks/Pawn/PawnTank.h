f2af258a97549d1a840167f9d15cad8887b37bd6
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2021-07-28 19:53:55 -0400

    Setup basic player input for movement

--- Source/ToonTanks/Pawn/PawnTank.h
+++ Source/ToonTanks/Pawn/PawnTank.h
@@ -36,4 +36,16 @@ private:
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* Camera;
+
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	float MoveSpeed = 100.f;
+	float RotateSpeed = 100.f;
+
+	void CalculateMoveInput(float value);
+	void CalculateRotateInput(float value);
+
+	void Move();
+	void Rotate();
 };
